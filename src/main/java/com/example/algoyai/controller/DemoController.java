c1555ef1e072236ab6a684819f9c9b4c23fee4f1
zoanna <zoanna5442@gmail.com>
2024-08-28 00:23:59 +0900

    test2

--- src/main/java/com/example/algoyai/controller/DemoController.java
+++ src/main/java/com/example/algoyai/controller/DemoController.java
@@ -2,12 +2,14 @@ package com.example.algoyai.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ai")
 public class DemoController {
 
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String test(){
         return "test";
     }
