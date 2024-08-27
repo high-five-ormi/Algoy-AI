e503cec835bec11919e49ba3c4583a46799ffd85
zoanna <zoanna5442@gmail.com>
2024-08-27 19:41:06 +0900

    nginx test

--- src/main/java/com/example/algoyai/controller/ChatController.java
+++ src/main/java/com/example/algoyai/controller/ChatController.java
@@ -2,8 +2,10 @@ package com.example.algoyai.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/ai")
 public class ChatController {
 
 	@GetMapping("/chat")
