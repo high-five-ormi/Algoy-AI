a65c7cb549ac0f6e28fe9eaef8fcc278fa49ed4a
JEESW <jeesw1221@gmail.com>
2024-09-02 18:05:35 +0900

    Chore: application.yml에 AI 서버 IP 환경 변수 설정 및 WebConfig.java에 Value로 url 불러오기

--- src/main/java/com/example/algoyai/config/WebConfig.java
+++ src/main/java/com/example/algoyai/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.algoyai.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+	@Value("${ai-frontend.url}")
+	private String frontendUrl;
+
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/ai/**")
-					.allowedOrigins("http://localhost:8081")  // 프론트엔드 서버 주소
+					.allowedOrigins(frontendUrl)  // 프론트엔드 서버 주소
 					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 					.allowedHeaders("*")
 					.allowCredentials(true);
